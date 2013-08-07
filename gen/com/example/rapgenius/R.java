63191d805b20398605be21616b05d9e0de18f1d2
thugnificent <josephthientran@gmail.com>
2013-08-07 02:18:33 -0400

    Added icons to the correct dp for each size.

--- gen/com/example/rapgenius/R.java
+++ gen/com/example/rapgenius/R.java
@@ -30,7 +30,6 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int action_favorite=0x7f0a0006;
@@ -54,28 +53,27 @@ public final class R {
         public static final int search=0x7f090002;
     }
     public static final class string {
-        public static final int Loading=0x7f070008;
         public static final int action_favorite=0x7f070006;
         public static final int action_search=0x7f070004;
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int default_search=0x7f070002;
-        public static final int dialog_clear_cache=0x7f070015;
-        public static final int dialog_remove_favorites=0x7f070010;
-        public static final int summary_cache_lyrics=0x7f07000d;
-        public static final int summary_clear_cache=0x7f070014;
-        public static final int summary_donate=0x7f070012;
-        public static final int summary_load_home=0x7f07000b;
-        public static final int summary_remove_favorites=0x7f07000f;
+        public static final int dialog_clear_cache=0x7f070014;
+        public static final int dialog_remove_favorites=0x7f07000f;
+        public static final int summary_cache_lyrics=0x7f07000c;
+        public static final int summary_clear_cache=0x7f070013;
+        public static final int summary_donate=0x7f070011;
+        public static final int summary_load_home=0x7f07000a;
+        public static final int summary_remove_favorites=0x7f07000e;
         public static final int title_activity_lyrics=0x7f070003;
         public static final int title_activity_search=0x7f070005;
-        public static final int title_activity_settings=0x7f070009;
-        public static final int title_cache_lyrics=0x7f07000c;
-        public static final int title_clear_cache=0x7f070013;
-        public static final int title_donate=0x7f070011;
+        public static final int title_activity_settings=0x7f070008;
+        public static final int title_cache_lyrics=0x7f07000b;
+        public static final int title_clear_cache=0x7f070012;
+        public static final int title_donate=0x7f070010;
         public static final int title_favorites=0x7f070007;
-        public static final int title_load_home=0x7f07000a;
-        public static final int title_remove_favorites=0x7f07000e;
+        public static final int title_load_home=0x7f070009;
+        public static final int title_remove_favorites=0x7f07000d;
     }
     public static final class style {
         /** 
