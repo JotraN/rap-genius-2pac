0632201eb044e489f706665f14642372cca3e267
Joseph Tran <josephthientran@gmail.com>
2014-01-05 11:44:27 -0800

    Aborted spannables with hebrew and surrounded with try and catches to try and catch spannable bug.

--- src/com/trasselback/rapgenius/helpers/LinkSelectableMovementMethod.java
+++ src/com/trasselback/rapgenius/helpers/LinkSelectableMovementMethod.java
@@ -1,5 +1,7 @@
 package com.trasselback.rapgenius.helpers;
 
+import java.util.regex.Pattern;
+
 import android.text.Selection;
 import android.text.Spannable;
 import android.text.method.LinkMovementMethod;
@@ -15,21 +17,33 @@ public class LinkSelectableMovementMethod extends LinkMovementMethod {
 
 	@Override
 	public void initialize(TextView widget, Spannable text) {
+		// Prevent crashes if Hebrew user
+		if(Pattern.matches("(\\p{InHebrew})+", text.toString())){
+			return;
+		}
 		Selection.setSelection(text, text.length());
 	}
 
 	@Override
 	public void onTakeFocus(TextView view, Spannable text, int dir) {
+		// Prevent crashes if Hebrew user
+		if(Pattern.matches("(\\p{InHebrew})+", view.toString())){
+			return;
+		}
 		if ((dir & (View.FOCUS_FORWARD | View.FOCUS_DOWN)) != 0) {
 			if (view.getLayout() == null) {
-				// Stop application from crashing for some users
-				try{
+				// Stop application from crashing for Arabic users
+				try {
 					Selection.setSelection(text, text.length());
-				} catch(StringIndexOutOfBoundsException e){
+				} catch (StringIndexOutOfBoundsException e) {
 				}
 			}
 		} else {
-			Selection.setSelection(text, text.length());
+			// Stop application from crashing for Arabic users
+			try {
+				Selection.setSelection(text, text.length());
+			} catch (StringIndexOutOfBoundsException e) {
+			}
 		}
 	}
 }
--- src/com/trasselback/rapgenius/helpers/RemoveUnderLine.java
+++ src/com/trasselback/rapgenius/helpers/RemoveUnderLine.java
@@ -1,5 +1,6 @@
 package com.trasselback.rapgenius.helpers;
 
+import java.util.regex.Pattern;
 
 import android.graphics.Color;
 import android.text.Spannable;
@@ -9,22 +10,31 @@ import android.widget.TextView;
 
 public class RemoveUnderLine {
 	public static void removeUnderline(TextView textView) {
-		Spannable text = (Spannable) textView.getText();
-		URLSpan[] spans = text.getSpans(0, text.length(), URLSpan.class);
-		for (URLSpan span : spans) {
-			int start = text.getSpanStart(span);
-			int end = text.getSpanEnd(span);
-			text.removeSpan(span);
-			// Grabs URL part of span and override text-decoration
-			span = new URLOverride(span.getURL());
-			text.setSpan(span, start, end, 0);
-			// Color links dark green if verified by artist
-			if (span.getURL().toString().contains("*"))
-				text.setSpan(
-						new ForegroundColorSpan(Color.argb(255, 38, 135, 31)),
-						start, end, 0);
+		// Prevent crashes if Hebrew user
+		if(Pattern.matches("(\\p{InHebrew})+", textView.toString())){
+			return;
 		}
-		textView.setText(text);
+
+		// Prevent crashes if Arabic user
+		try {
+			Spannable text = (Spannable) textView.getText();
+			URLSpan[] spans = text.getSpans(0, text.length(), URLSpan.class);
+			for (URLSpan span : spans) {
+				int start = text.getSpanStart(span);
+				int end = text.getSpanEnd(span);
+				text.removeSpan(span);
+				// Grabs URL part of span and override text-decoration
+				span = new URLOverride(span.getURL());
+				text.setSpan(span, start, end, 0);
+				// Color links dark green if verified by artist
+				if (span.getURL().toString().contains("*"))
+					text.setSpan(
+							new ForegroundColorSpan(Color
+									.argb(255, 38, 135, 31)), start, end, 0);
+			}
+			textView.setText(text);
+		} catch (StringIndexOutOfBoundsException ex) {
+		}
 		// Makes links click-able
 		textView.setMovementMethod(new LinkSelectableMovementMethod());
 	}
