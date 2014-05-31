3c9aae334fe9769f736aeaab2abc956b2dc43b10
Jotra N <josephthientran@gmail.com>
2014-05-31 11:17:48 -0400

    Resize() takes into account window width as well as resizes onresize.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function Resize(){
 	var content = document.getElementById("content");
-	if(navigator.userAgent.match(/Android|iPad|iPhone|iPod|Windows Phone/i) != null){
+	if(navigator.userAgent.match(/Android|iPad|iPhone|iPod|Windows Phone/i) != null || window.innerWidth <= 1380){
 		content.style.width = "80%";
 	} else {
 		content.style.width = "50%";
