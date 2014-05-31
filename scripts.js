fb4747785ce8547370bf58e50612b4fe90cb291d
Jotra N <josephthientran@gmail.com>
2014-05-31 11:09:10 -0400

    Added windows phone to Resize function.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function Resize(){
 	var content = document.getElementById("content");
-	if(navigator.userAgent.match(/android|ipad|iphone|ipod/i) != null){
+	if(navigator.userAgent.match(/Android|iPad|iPhone|iPod|Windows Phone/i) != null){
 		content.style.width = "80%";
 	} else {
 		content.style.width = "50%";
