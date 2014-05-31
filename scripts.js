0976c1b018a57e6f6258ec0e2fdb04387f4682a2
Jotra N <josephthientran@gmail.com>
2014-05-31 11:03:41 -0400

    Added closing parenthesis for if statement in Resize function.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function Resize(){
 	var content = document.getElementById("content");
-	if(navigator.userAgent.match(/android|ipad|iphone|ipod/i) != null{
+	if(navigator.userAgent.match(/android|ipad|iphone|ipod/i) != null){
 		content.style.width = "80%";
 	} else {
 		content.style.width = "50%";
